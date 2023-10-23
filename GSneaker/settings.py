db74edef5175e8c4291a6320e637629027bdebcd
nthieu211 <trungghieuu4713@gmail.com>
2023-10-24 03:54:59 +0700

    settings

--- GSneaker/settings.py
+++ GSneaker/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "gotest.nth.id.vn",
-    "127.0.0.1",
+    "159.65.7.110",
     "localhost",
 ]
 
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "simpleCart",
+    "simpleCart.apps.SimplecartConfig",
 ]
 
 MIDDLEWARE = [
@@ -130,6 +130,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
